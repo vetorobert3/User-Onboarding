9016d5dbe31ca289435c6d254e360b8ce9a5b84c
Veto Ramirez <vetorobert3@gmail.com>
2019-10-10 17:26:20 -0700

    Started form.js.

---
+++ onboarding/src/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+cosnt Form = props => {
+
+    return (
+        <div>
+            <form>
+                <label>
+                    <input></input>
+                </label>
+            </form>
+        </div>
+
+    )
+}
+
